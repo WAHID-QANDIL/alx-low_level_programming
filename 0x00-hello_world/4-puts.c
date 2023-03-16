60957fb25eb62988a719547719eef52869041057
wahid <1213089@dtu.edu.eg>
2023-03-16 16:25:15 -0700

    Write a script that runs a C file through the preprocessor and save the result into another file.
    The C file name will be saved in the variable main.c
    The output should be saved in the file c
    Write a C program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line.

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ * main- Sart from here
+ *
+ * desception: print \"Programming is like building a multilingual puzzle\"
+ * Return: 0 (Succsess)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle);
+
+	return (0);
+}
