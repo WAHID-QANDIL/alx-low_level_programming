0a56c268689e60492a108faf8c1ec280be08f841
wahid <1213089@dtu.edu.eg>
2023-03-21 13:55:42 -0700

    #include <unistd.h>
    /**
     * _putchar - writes the character c to stdout
     * @c: The character to print
     *
     * Return: On success 1.
     * On error, -1 is returned, and errno is set appropriately.
     */
    int _putchar(char c)
    {
    return (write(1, &c, 1));
    }
    Write a function that computes the absolute value of an integer.

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include"main.h"
+/**
+ * _abs - return absolute value for n
+ * @n: input function
+ * Return: return abs for n if n < 0 alse return n
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		n = n * -1;
+		return (n);
+	}
+	else
+	{
+		return (n);
+	}
+}
