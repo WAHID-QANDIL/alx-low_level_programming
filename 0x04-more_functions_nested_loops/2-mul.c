bd0dad1e229b62e8896c08ce2a6a05e18b526d91
wahid <1213089@dtu.edu.eg>
2023-03-23 08:33:15 -0700

    Fix the code in 2-largest_number.c so that it correctly prints out the largest of three numbers, no matter the case. Write a function that multiplies two integers.
    Prototype: int mul(int a, int b);

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - this fun take two parameters and return the mul
+ *
+ * @a: number one
+ * @b:number two
+ * Return: return the multiple of these numbers (a & b)
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
