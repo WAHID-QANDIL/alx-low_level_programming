60b4157eabfb0ab4f609efedc67943567f1d2dbc
wahid <1213089@dtu.edu.eg>
2023-03-23 19:12:56 -0700

    Write a function that prints the numbers, from 0 to 9, followed by a new line.
    Prototype: void print_most_numbers(void);
    Do not print 2 and 4
    You can only use _putchar twice in your code
    Write a function that prints a square, followed by a new line.
    
    Prototype: void print_square(int size);
    You can only use _putchar function to print
    Where size is the size of the square
    If size is 0 or less, the function should print only a new line
    Use the character # to print the square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_square - print square of #s
+ *
+ * @size: the size of square of #s
+ * Return: no return value
+ */
+void print_square(int size)
+{
+	int a, b;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (a = 0; a < size; a++)
+		{
+			for (b = 0; b < size; b++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
