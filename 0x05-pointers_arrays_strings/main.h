3201488b811826bb514788f8cfe284fe151da6bd
wahid <1213089@dtu.edu.eg>
2023-03-27 13:07:45 -0700

    fix

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -9,7 +9,7 @@ void rev_string(char *s);
 void rev_string(char *s);
 void puts_half(char *str);
 void print_array(int *a, int n);
-char *_strcpy(char *dest,const char *src);
+char *_strcpy(char *dest, char *src);
 int _putchar(char c);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
