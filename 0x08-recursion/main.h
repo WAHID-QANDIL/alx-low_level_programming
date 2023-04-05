4045748fd131c592e9ffb1bb3fe33c010d6c209d
wahid <1213089@dtu.edu.eg>
2023-04-05 15:13:52 -0700

    fix main.h file

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -8,7 +8,21 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
+int is_palindrome(char *s);
+int wildcmp(char *s1, char *s2);
 
+
+/* This a header file containing declarations
+ * for prototype functions defined in the 0x08-recursion
+ * tasks and directory
+ * This is like creating my own library with declared functions
+ * The tasks defines  the functions
+ *
+ *
+ * enclosing the files  contentinbetween ifndef,define and endif,
+ * stops the compiler from processing the file twice if its included twice,
+ * if not so, this will cause an error
+ * */
 
 
 
